0c7dd2433702f3584deaac05ff4702da949415f9
felix <1152062134@qq.com>
2021-04-01 19:24:41 +0800

    init project

---
+++ foodie-dev-mapper/src/main/java/com/felix/my/mapper/MyMapper.java
@@ -0,0 +1,10 @@
+package com.felix.my.mapper;
+
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+/**
+ * 继承自己的MyMapper
+ */
+public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
+}
